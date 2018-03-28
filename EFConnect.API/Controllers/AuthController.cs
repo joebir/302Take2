25acbfbe8b4275121c1c75a67404a24c753d0994
Joseph Bir <joe.bir.code@gmail.com>
2018-03-28 14:57:15 -0400

    Completed work through end of 2.6; encountering 404 when testing.

--- EFConnect.API/Controllers/AuthController.cs
+++ EFConnect.API/Controllers/AuthController.cs
@@ -1,4 +1,7 @@
+using System.Threading.Tasks;
 using EFConnect.Contracts;
+using EFConnect.Data.Entities;
+using EFConnect.Models.User;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EFConnect.API.Controllers
@@ -10,7 +13,24 @@ namespace EFConnect.API.Controllers
         public AuthController(IAuthService authService)
         {
             _authService = authService;
+        }
 
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register([FromBody] UserForRegister userForRegister)
+        {
+            userForRegister.Username = userForRegister.Username.ToLower();
+
+            if (await _authService.UserExists(userForRegister.Username))
+                return BadRequest("Username is already taken");
+            
+            var userToCreate = new User
+            {
+                Username = userForRegister.Username
+            };
+
+            var createUser = await _authService.Register(userToCreate, userForRegister.Password);
+
+            return StatusCode(201);
         }
     }
 }
